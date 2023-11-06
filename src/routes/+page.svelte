9fd15351e723e4e1d8bd81ff15a3496403dc85e1
ItaIta123 <66623308+ItaIta123@users.noreply.github.com>
2023-11-05 22:44:33 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<ul>
+	{#each data.users as user}
+		<li>{user.name}</li>
+	{/each}
+</ul>
